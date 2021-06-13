b6ee91c9c136eb069235496934321b2eada897bd
Biruk Alamirew <biruk.alamirew77@gmail.com>
2021-06-13 11:53:47 +0300

    created customers orders app

--- gebeya/settings.py
+++ gebeya/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom apps
     'shop',
     'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
